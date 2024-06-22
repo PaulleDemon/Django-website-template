faf1e8bb655f7766e0254b5cbbc4c28bde6e0953
paul <paul.sajo.official@gmail.com>
2024-06-22 21:26:12 +0530

    configured for vercel

--- project/wsgi.py
+++ project/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
+
+app = application # for vercel, as it looks for app and not application
